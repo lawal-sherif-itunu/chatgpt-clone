efc49c1a0077ac4a9af3e4e6c091f1c2972eb184
Sherif Lawal <lawalsherif0000@gmail.com>
2025-02-08 14:35:00 +0100

    html-css update

---
+++ sandbox.js
@@ -0,0 +1,5 @@
+document.querySelector(".continue").addEventListener("click", function () {
+  document.querySelector(".typing-container").style.display = "block";
+  document.querySelector(".chat-container").style.display = "block";
+  document.querySelector(".info_box").style.display = "none";
+});
