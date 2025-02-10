994de19a85cf0dbf4c07b11673a738d8de3e424d
Sherif Lawal <lawalsherif0000@gmail.com>
2025-02-10 22:28:53 +0100

    interface update

--- script.js
+++ script.js
@@ -7,9 +7,8 @@ document.querySelector(".continue").addEventListener("click", function () {
 const chatInput = document.querySelector("#chat-input");
 const sendButton = document.querySelector("#send-btn");
 const chatContainer = document.querySelector(".chat-container");
-const themeButton = document.querySelector("#theme-btn");
+const themeButton = document.querySelectorAll("#theme-btn");
 const deleteButton = document.querySelector("#delete-btn");
-
 
 let userText = null;
 // API Configuration
@@ -21,16 +20,18 @@ const loadDatafromLocalstorage = () => {
   const themeColor = localStorage.getItem("theme-color");
 
   document.body.classList.toggle("light_mode", themeColor === "light_mode");
-  themeButton.innerText = document.body.classList.contains("light_mode") ? "dark_mode" : "light_mode";
+  themeButton.innerText = document.body.classList.contains("light_mode")
+    ? "dark_mode"
+    : "light_mode";
 
   const defaultText = `<div class="default-text">
                         <h1>ChatGPT Clone</h1>
                         <p> Start a conversation and explore the power of AI. <br> Your chat history will be displayed here.</p>
-                      </div>`
+                      </div>`;
 
   chatContainer.innerHTML = localStorage.getItem("all-chats") || defaultText;
   // chatContainer.scrollTo(0, chatContainer.scrollHeight);
-}
+};
 
 loadDatafromLocalstorage();
 
@@ -40,10 +41,9 @@ const createElement = (html, className) => {
   chatDiv.classList.add("chat", className);
   chatDiv.innerHTML = html;
   return chatDiv; //Return the created chat div
-}
+};
 
 const getChatResponse = async (incomingChatDiv) => {
-
   const textElement = document.createElement("p"); //Get the text element
 
   try {
@@ -51,11 +51,13 @@ const getChatResponse = async (incomingChatDiv) => {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        contents: [{
-          role: "user",
-          parts: [{ text: userText }]
-        }]
-      })
+        contents: [
+          {
+            role: "user",
+            parts: [{ text: userText }],
+          },
+        ],
+      }),
     });
 
     const data = await response.json();
@@ -65,22 +67,23 @@ const getChatResponse = async (incomingChatDiv) => {
     textElement.textContent = apiResponse;
   } catch (error) {
     textElement.classList.add("error");
-    textElement.textContent = "Oops! Something went wrong while retrieving the response. Please try again.";
+    textElement.textContent =
+      "Oops! Something went wrong while retrieving the response. Please try again.";
   }
 
-  // remove the typing animation, append the paragraph element and save the chats to local storage  
+  // remove the typing animation, append the paragraph element and save the chats to local storage
   incomingChatDiv.querySelector(".typing-animation").remove();
   incomingChatDiv.querySelector(".chat-details").appendChild(textElement);
   // chatContainer.scrollTo(0, chatContainer.scrollHeight);
-  localStorage.setItem("all-chats", chatContainer.innerHTML)
-}
+  localStorage.setItem("all-chats", chatContainer.innerHTML);
+};
 const copyResponse = (copyBtn) => {
   // Copy the text content of the response to the clipboard
   const responseTextElement = copyBtn.parentElement.querySelector("p");
   navigator.clipboard.writeText(responseTextElement.textContent);
   copyBtn.textContent = "done";
-  setTimeout(() => copyBtn.textContent = "content_copy", 1000);
-}
+  setTimeout(() => (copyBtn.textContent = "content_copy"), 1000);
+};
 
 const showTypingAnimation = () => {
   const html = `<div class="chat-content">
@@ -100,7 +103,7 @@ const showTypingAnimation = () => {
   chatContainer.appendChild(incomingChatDiv);
   //chatContainer.scrollTo(0, chatContainer.scrollHeight);
   getChatResponse(incomingChatDiv);
-}
+};
 
 const handleOutgoingChat = () => {
   userText = chatInput.value.trim(); // Fetches chatinput value and trim extra spaces
@@ -111,7 +114,7 @@ const handleOutgoingChat = () => {
   const html = `<div class="chat outgoing">
     <div class="chat-content">
       <div class="chat-details">
-        <img src="images/sherif.jpeg" alt="" />
+        <img src="images/user.png" alt="" />
         <p></p>
       </div>
       </div>
@@ -125,23 +128,23 @@ const handleOutgoingChat = () => {
   // chatContainer.scrollTo(0, chatContainer.scrollHeight);
   setTimeout(showTypingAnimation, 500);
   //console.log(userText);
-}
+};
 deleteButton.addEventListener("click", () => {
   // Remove the chats from local storage and call the loadDatafromLocalstorage function
   if (confirm("Are you sure you want to delete all the chats?")) {
     localStorage.removeItem("all-chats");
     loadDatafromLocalstorage();
   }
-})
+});
 
 themeButton.addEventListener("click", () => {
   // Toggle the body's class for the theme mode and save the updated theme to the local storage
   document.body.classList.toggle("light_mode");
   localStorage.setItem("theme-color", themeButton.innerText);
-  themeButton.innerText = document.body.classList.contains("light_mode") ? "dark_mode" : "light_mode";
+  themeButton.innerText = document.body.classList.contains("light_mode")
+    ? "dark_mode"
+    : "light_mode";
 });
-
-
 
 chatInput.addEventListener("input", () => {
   // Adjust the height of the input field dynamically based on its content
